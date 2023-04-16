b6172f0c0661ee76b1b401b9329a78dbd79b6627
Anuj-Kafaliya <71915606+Anuj-Kafaliya@users.noreply.github.com>
2023-04-16 19:56:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const env = require('dotenv');
+const mongoose = require('mongoose');
+const userRouter = require('./routing/user-routes');
+const postRouter = require('./routing/post-routes');
+env.config();
+
+//middlewares
+app.use(express.json());
+app.use('/user', userRouter);
+app.use('/posts', postRouter);
+
+
+//connection to MongoDB
+mongoose.set('strictQuery', false);
+mongoose.connect(
+    `mongodb+srv://admin:fGeCe9VB8xbV7LXo@cluster0.gtge4qp.mongodb.net/?retryWrites=true&w=majority`
+).then(() => {
+    app.listen(4040, () => {
+        console.log('listening to 4040');
+    });
+}).catch((err) => {
+    console.log(err);
+});
+
+
+
